bd727b62fb6ed034968b6c5e2e38af08297b1c61
Yujin Han <yuhan@tcd.ie>
2025-05-02 15:08:39 +0100

    Add restriction and prolongation operators

--- multigrid.cpp
+++ multigrid.cpp
@@ -42,3 +42,65 @@ int compute_lmax(int N, int coarse_N)
         ++levels;
     return levels;
 }
+
+// Restriction: Full-weighting from fine (N) to coarse (N/2)
+std::vector<double> restrictResidual(const std::vector<double> &r_fine, int N_fine)
+{
+    int N_coarse = N_fine / 2;
+    std::vector<double> r_coarse((N_coarse + 1) * (N_coarse + 1), 0.0);
+
+    for (int i = 1; i < N_coarse; ++i)
+    {
+        for (int j = 1; j < N_coarse; ++j)
+        {
+            int ic = i * (N_coarse + 1) + j;
+
+            int fi = 2 * i;
+            int fj = 2 * j;
+            int fc = fi * (N_fine + 1) + fj;
+
+            r_coarse[ic] = 0.25 * r_fine[fc] + 0.125 * (r_fine[fc - 1] + r_fine[fc + 1] + r_fine[fc - (N_fine + 1)] + r_fine[fc + (N_fine + 1)]) + 0.0625 * (r_fine[fc - (N_fine + 2)] + r_fine[fc - (N_fine)] + r_fine[fc + (N_fine + 2)] + r_fine[fc + (N_fine)]);
+        }
+    }
+
+    return r_coarse;
+}
+
+// Prolongation: Bilinear interpolation from coarse (N) to fine (2N)
+std::vector<double> prolongCorrection(const std::vector<double> &e_coarse, int N_coarse)
+{
+    int N_fine = 2 * N_coarse;
+    std::vector<double> e_fine((N_fine + 1) * (N_fine + 1), 0.0);
+
+    for (int i = 0; i <= N_coarse; ++i)
+    {
+        for (int j = 0; j <= N_coarse; ++j)
+        {
+            int coarse_idx = i * (N_coarse + 1) + j;
+            int fi = 2 * i;
+            int fj = 2 * j;
+            int fine_idx = fi * (N_fine + 1) + fj;
+            e_fine[fine_idx] = e_coarse[coarse_idx];
+        }
+    }
+
+    // Interpolate horizontally and vertically
+    for (int i = 1; i < N_fine; i += 2)
+    {
+        for (int j = 0; j <= N_fine; ++j)
+        {
+            int idx = i * (N_fine + 1) + j;
+            e_fine[idx] = 0.5 * (e_fine[(i - 1) * (N_fine + 1) + j] + e_fine[(i + 1) * (N_fine + 1) + j]);
+        }
+    }
+    for (int i = 0; i <= N_fine; ++i)
+    {
+        for (int j = 1; j < N_fine; j += 2)
+        {
+            int idx = i * (N_fine + 1) + j;
+            e_fine[idx] = 0.5 * (e_fine[i * (N_fine + 1) + (j - 1)] + e_fine[i * (N_fine + 1) + (j + 1)]);
+        }
+    }
+
+    return e_fine;
+}
--- multigrid.h
+++ multigrid.h
@@ -19,12 +19,27 @@ std::vector<double> multigrid_solver(int N, std::vector<double> x, const std::ve
 /**
  * @brief One recursive V-cycle
  */
-std::vector<double> Vcycle(int N, std::vector<double> x, const std::vector<double> &b,
-                           double omega, int nu, int level, int lmax);
+std::vector<double> Vcycle(int N, std::vector<double> x, const std::vector<double> &b, double omega, int nu, int level, int lmax);
 
 /**
  * @brief Compute number of levels from fine grid N down to coarse_N
  */
 int compute_lmax(int N, int coarse_N);
+
+/**
+ * @brief Restrict residual from fine grid (N) to coarse grid (N/2)
+ * @param r_fine Residual vector on fine grid
+ * @param N_fine Fine grid size
+ * @return Restricted residual on coarse grid
+ */
+std::vector<double> restrictResidual(const std::vector<double> &r_fine, int N_fine);
+
+/**
+ * @brief Prolong correction from coarse grid (N) to fine grid (2N)
+ * @param e_coarse Error correction on coarse grid
+ * @param N_coarse Coarse grid size
+ * @return Prolongated correction on fine grid
+ */
+std::vector<double> prolongCorrection(const std::vector<double> &e_coarse, int N_coarse);
 
 #endif // MULTIGRID_H
